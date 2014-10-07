d25f292be3685350ee2a51f5cba5ae815b50970c
Joseph Paisley <jpaisley@lp01826.openbet>
2014-10-07 22:58:40 +0100

    Adding new projects

--- CollectionStudy/src/main/java/Startup.java
+++ CollectionStudy/src/main/java/Startup.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 import com.joesystem.collectionstudy.*;
+import com.joesystem.generics.*;
+import java.util.*;
+import com.joesystem.equalsstudy.*;
 /**
  *
  * @author jpaisley
@@ -13,6 +16,21 @@ public class Startup {
     public static void main(String[] args){
         
         ListCreator c = new ListCreator();
+        
+        System.out.println(GenericsImpl.getNum(5));
+        System.out.println(GenericsImpl.getNum("to"));
+        
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+       
+        for (int i=0; i<20; i++)
+            arr.add(i);
+        
+        System.out.println(GenericsImpl.getCount(arr));
+        
+        JoeObj jo = new JoeObj(1,2);
+        jo.hashCode();
+        
         
         
     }
--- ConcurrencyMaven/src/main/java/com/joesystem/concurrencymaven/ConcurrencyStartup.java
+++ ConcurrencyMaven/src/main/java/com/joesystem/concurrencymaven/ConcurrencyStartup.java
@@ -6,6 +6,8 @@
 
 package com.joesystem.concurrencymaven;
 
+import prodcons.*;
+import com.joesystem.concurrencymaven.blocking.*;
 /**
  *
  * @author jpaisley
@@ -13,15 +15,58 @@ package com.joesystem.concurrencymaven;
 public class ConcurrencyStartup {
     
     
+    SynchronizedCounter s1 = new SynchronizedCounter();
+    
+    
     public static void main(String[] args){
         
-       
+       /*
          Runnable myrun = new ThreadBasic(); 
          Thread t = new Thread(myrun);
          t.start();
          t.interrupt();
+         System.out.println("Interrupted:"+t.isInterrupted());
    
+         StartTest t2 = new ConcurrencyStartup().new StartTest();
+         t2.startTesting();
+       */
+        // Drop drop = new Drop();
+        //(new Thread(new Producer(drop))).start();
+        //(new Thread(new Consumer(drop))).start();
         
+        BlockingQueueExample bl = new BlockingQueueExample();
+        
+         
     }
+    
+     class StartTest {
+    
+        public void startTesting() {
+            System.out.println("Start Thread  1");
+           new Thread(new Runnable() {
+                @Override
+                public void run(){
+                   for (int i=0; i<20;i++){
+                       s1.increment();
+                   }
+                }
+                }).start();
+         //System.out.println("value:"+ s1.value());
+           
+            System.out.println("Start Thread 2");
+           new Thread(new Runnable() {
+                @Override
+                public void run(){
+                    for (int i=0; i<20;i++){
+                       s1.decrement();
+                   }
+                }
+                }).start();
+        }
+        
+       
+       
+     }
+    
     
 }
